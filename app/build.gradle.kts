3528a721f828d2ff35719524e315c575bf3b7f37
gmzucolo <gustavo.zucolo@hotmail.com>
2024-04-15 20:48:33 -0300

    classe de teste

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,7 +53,10 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.2")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:okhttp:4.9.0")
     testImplementation("junit:junit:4.13.2")
+    testImplementation("com.google.truth:truth:1.4.2")
+    testImplementation("com.squareup.okhttp3:mockwebserver:4.9.0")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
